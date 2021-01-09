db6edb5917b25046310ae6059cb31fa7c5184358
ryunakae <ryunakae@gmail.com>
2021-01-09 14:56:19 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <input type="text" v-model="n">
+      <button @click="leapYear">調べる</button>
+      <p>{{message}}</p>
+    </div>
+    <div class="contener">
+      <p @mouseover="hover" @mouseleave="leave">アコーディオン</p>
+      <div v-if="hoverFlag">
+        アコーディオンの中身が入ります。
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      hoverFlag: false,
+      message: "",
+      n: ""
+    };
+  },
+  method: {
+    hover(){
+      this.hoverFlag = true
+    },
+    leave(){
+      this.hoverFlag = false
+    },
+    leapYear() {
+      if (this.n % 4 == 0 && this.n % 100 !== 0 || this.n % 400 == 0) {
+        this.message = "閏年"
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +53,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.contener {
+  border: solid 1px;
+  width: 400px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+.contener p {
+  background-color: #d4d4d4;
+  margin: 0;
+  padding: 10px;
+}
+.contener div {
+  line-height: 100px;
 }
 </style>
